5e896fc15e83619b85e9d976e726ff50dc97a8f6
mberoakoko <mberoakoko@gmail.com>
2025-06-05 23:56:55 +0200

    Started working on the sliding window method

--- src/utils/pipelines.hpp
+++ src/utils/pipelines.hpp
@@ -11,9 +11,9 @@
 #include "opencv2/features2d/features2d.hpp"
 
 namespace PipeLine {
-    class lane_detection_pipeline {
+    class ILaneDetectionPipeline {
     public:
-        virtual ~lane_detection_pipeline() = default;
+        virtual ~ILaneDetectionPipeline() = default;
         virtual auto execute(const cv::Mat& input_image) -> cv::Mat = 0;
     };
 
@@ -65,20 +65,59 @@ namespace PipeLine {
         std::uint8_t kernel_size_;
     };
 
-    class GradientFilterPipeline : public lane_detection_pipeline {
+    class GradientFilterPipeline : public ILaneDetectionPipeline {
+    public:
         auto execute(const cv::Mat &input_image) -> cv::Mat override;
     };
 
-    class CannyGaussianFilterPipeline: public lane_detection_pipeline {
+    class CannyGaussianFilterPipeline: public ILaneDetectionPipeline {
+    public:
         auto execute(const cv::Mat &input_image) -> cv::Mat override;
+    };
+
+
+    class SlidingWindowPipeline final: public ILaneDetectionPipeline {
+    public:
+        struct HistogramEdges {
+            cv::Mat histogram_data;
+            std::uint16_t left_edge;
+            std::uint16_t right_edge;
+        };
+
+        static auto calculate_histogram_edges(const cv::Mat &input_image) -> HistogramEdges {
+            std::uint16_t mid_point_0_axis = ::round(input_image.rows()/2);
+            std::uint16_t mid_point_1_axis = ::round(input_image.cols()/2);
+            cv::Mat lower_region = input_image(
+                cv::Range(mid_point_0_axis, input_image.rows()), cv::Range::all()
+                );
+            cv::Mat hist_data ;
+            cv:std::reduce(lower_region, hist_data, 0, cv::REDUCE_SUM);
+            auto min_along_axis = [](const cv::Mat& data_mat) -> std::uint16_t {
+                cv::Point min_loc;
+                cv::minMaxLoc(data_mat, nullptr, nullptr, &min_loc, nullptr);
+                return static_cast<std::uint16_t>(min_loc.x);
+            };
+            ;
+            return {
+                .histogram_data = hist_data,
+                .left_edge = min_along_axis(hist_data(cv::Range(0, mid_point_0_axis), cv::Range::all())),
+                .right_edge = min_along_axis(hist_data(cv::Range(mid_point_0_axis, hist_data.rows()), cv::Range::all()))
+            };
+        };
+
+        auto execute(const cv::Mat &input_image) -> cv::Mat override {
+            auto [histogram_data, left_edge, right_edge] =
+                    SlidingWindowPipeline::calculate_histogram_edges(input_image);
+            return input_image.clone();
+        };
     };
 
     // TODO: use tag dispatching to enable the dispatching of various lane detection pipelines
 
-    class PrototypePipeline: public lane_detection_pipeline {
+    class PrototypePipeline: public ILaneDetectionPipeline {
     public:
         explicit PrototypePipeline(std::unique_ptr<IEdgeDetection>detector)
-            :lane_detection_pipeline(),
+            :ILaneDetectionPipeline(),
             detector_(std::move(detector)) {
 
         }
